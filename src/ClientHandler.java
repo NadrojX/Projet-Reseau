cb83fa4114e169fce8d11bf8c7adf8bc4b6213ed
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2023-03-17 16:01:36 +0100

    Mise en place de l'id pour le message + modification du message reçu enregistrer en StringBuilder pour pouvoir le conserver.

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -2,9 +2,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ClientHandler extends Thread{
     private final Socket socket;
+    private final HashMap<String, Long> messagesID = new HashMap<>();
+    private final HashMap<String, String> messagesAuthor = new HashMap<>();
+
+    private long id = 0;
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
@@ -21,13 +26,16 @@ public class ClientHandler extends Thread{
 
                 String header = requestLines[0];
                 String author = requestLines[1];
+                StringBuilder message = new StringBuilder();
 
                 if (header.equals("PUBLISH")) {
                     System.out.println(header);
                     if(requestLines.length >= 3 && requestLines.length <= 259) {
                         for (int i = 2; i < requestLines.length; i++) {
-                            System.out.print(requestLines[i] + " ");
+                            message.append(requestLines[i]).append(" ");
                         }
+                        messagesID.put(message.toString(), updateId());
+                        messagesAuthor.put(message.toString(), author);
                         out.println("OK\r\n\r\n");
                     } else {
                         out.println("ERROR\\r\\nThere is an error your message need to make 256 characters maximum.\\r\\n");
@@ -38,6 +46,10 @@ public class ClientHandler extends Thread{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private synchronized long updateId() {
+        return id++;
     }
 
 }
